651b4ded25190fe3eb56b772d202b0b110034eef
Calderani <joao.filho@tecnobank.com.br>
2024-11-25 19:09:54 -0300

    criacao estrutura

---
+++ src/app/features/regulatorio/palavra-chave/palavra-chave.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-palavra-chave',
+  standalone: true,
+  imports: [],
+  templateUrl: './palavra-chave.component.html',
+  styleUrl: './palavra-chave.component.scss'
+})
+export class PalavraChaveComponent {
+
+}
---
+++ src/app/shared/components/file-card/file-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './file-card.component.html',
+  styleUrl: './file-card.component.scss'
+})
+export class FileCardComponent {
+
+}
