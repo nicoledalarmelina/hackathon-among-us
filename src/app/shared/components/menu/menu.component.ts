e00219df6be4701840beaf807c3293fb40ad4d22
Nicole do Vale <nicole.dalarmelina@gmail.com>
2024-11-26 21:58:06 -0300

    fix: comportamento menu

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,16 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
 
   constructor(private router: Router) {}
+
+  selecionado: string = "regulatorio";
 
   goTo(link: string) {
     this.router.navigate([link]);
