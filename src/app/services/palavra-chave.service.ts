ee841a1713430725dbc18f705a7c83611a98e982
Nicole do Vale <nicole.dalarmelina@gmail.com>
2024-11-28 15:54:35 -0300

    feat: estrutura environments e service

--- src/app/services/palavra-chave.service.ts
+++ src/app/services/palavra-chave.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environments';
+import { ObterPalavrasChaveResponse } from '../core/responses/palavra-chave/obter-palavras-chave.response';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PalavraChaveService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  obterPalavrasChave() {
+    return this.http.get(`${environment.urlObterPalavraChave}`)
+      .pipe(map(data => this.transformToObterPalavrasChave(data)));
+  }
+
+  private transformToObterPalavrasChave(data: any) {
+    let response: ObterPalavrasChaveResponse = new ObterPalavrasChaveResponse();
+
+    if (data) {
+      response.id = data.result.id;
+      response.palavra = data.result.palavra;
+      response.criadaPor = data.result.criadaPor;
+      response.criadaEm = data.result.criadaEm;
+      response.ativa = data.result.ativa;
+    }
+
+    return response;
+  }
 }
---
+++ src/environments/environments.ts
@@ -0,0 +1,17 @@
+const regulatorio = "https://regulatorio/api"; //TODO
+const dashboard = "https://dashboard/api"; //TODO
+
+export const environment = {
+    //#region PALAVRA-CHAVE
+
+    urlObterPalavraChave: `${regulatorio}`,
+
+
+    //#endregion
+
+    //#region DASHBOARD
+
+    urlObterListaDocumentos: `${dashboard}/documentos`
+
+    //#endregion
+}
