e9c5d9f2920182c19badd7c914e6b49f6be64bbf
Calderani <joao.filho@tecnobank.com.br>
2024-11-28 09:22:03 -0300

    feat: ajustes de css do angular material e ajuste dialog

--- src/app/shared/components/keyword-dialog/keyword-dialog.component.ts
+++ src/app/shared/components/keyword-dialog/keyword-dialog.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-keyword-dialog',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, MatDialogModule],
   templateUrl: './keyword-dialog.component.html',
   styleUrl: './keyword-dialog.component.scss'
 })
-export class KeywordDialogComponent {
+export class KeywordDialogComponent implements OnInit {
+  formKeyword: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.initializeForm()
+  }
+
+  initializeForm() {
+    this.formKeyword = this.fb.group({
+      keyword: [null, [Validators.required, Validators.minLength(3)]]
+    })
+  }
 }
