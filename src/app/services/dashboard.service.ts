f48485d0b4019b5572f1c15192cc1e936c2910fd
Calderani <joao.filho@tecnobank.com.br>
2024-11-28 15:55:56 -0300

    feat: service dashboard

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  obterDadosUltimos30DiasGraficoBarra() {
+  }
 }
